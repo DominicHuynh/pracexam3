8d4e03e41622fec724772025ed5449fa906db2e7
DominicHuynh <phucvbin2005@gmail.com>
2024-05-02 17:04:38 +0930

    d

--- main-3-1.cpp
+++ main-3-1.cpp
@@ -4,9 +4,9 @@ using namespace std;
 #include "Fleet.h"
 
 int main(){
-  Fleet a;
+    Fleet a;
   for(int i=0;i<5;i++){
-    cout << a.get_fleet()[i] << endl;
+    cout << (a.get_fleet()[i]) -> get_emissions() << endl;
   }
 
 }
