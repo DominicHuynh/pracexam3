f2c69068049f92fdc3ce3a5ae4a1ed515a6a2d82
DominicHuynh <phucvbin2005@gmail.com>
2024-05-02 16:19:41 +0930

    d

---
+++ Tesla.cpp
@@ -0,0 +1,43 @@
+
+#include<iostream>
+using namespace std;
+#include "Tesla.h"
+
+Tesla::Tesla(): Car(){
+  this->batteryPercentage = 100;
+};
+
+Tesla::Tesla(char model,int price): Car(price){
+  this->price = price;
+  this->batteryPercentage = 100;
+}
+
+char Tesla::get_model(){return this-> model;}
+
+float Tesla::get_batteryPercentage(){return this-> batteryPercentage;}
+
+void Tesla::set_model(char model){this->model = model;}
+
+void Tesla::set_batteryPercentage(int batteryPercentage){
+  if (batteryPercentage < 0){
+    this->batteryPercentage = 0;
+  }
+  else{
+    if(batteryPercentage > 100){
+      this->batteryPercentage = 100;
+    }
+    else{
+      this->batteryPercentage = batteryPercentage;
+    }
+  }
+}
+
+void Tesla::chargeBattery(int mins){
+  this->batteryPercentage = min(float(100), float(this->batteryPercentage+0.5*float(mins)));
+}
+
+void Tesla::drive(int kms){ 
+  float kms_max_drive = this->batteryPercentage / 0.2;
+  this->emissions += min(float(kms),kms_max_drive)*74;
+  this->batteryPercentage -= min(float(kms),kms_max_drive)*0.2;
+}
---
+++ main-2-1.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+
+#include "Tesla.h"
+
+
+int main(){
+  Tesla modelS('S', 300);
+  modelS.set_batteryPercentage(10);
+  modelS.drive(33);
+  cout << modelS.get_batteryPercentage() << endl;
+
+}
